1e59254058d1e2b3ff21e4af9c9315b2b7ef65bf
not-sure <djuric2103@gmail.com>
2020-03-30 22:21:42 +0200

    1

--- src/main/scala/ch/epfl/dias/cs422/Main.scala
+++ src/main/scala/ch/epfl/dias/cs422/Main.scala
@@ -1,21 +1,19 @@
 package ch.epfl.dias.cs422
 
 import ch.epfl.dias.cs422.helpers.builder.Factories
-import ch.epfl.dias.cs422.helpers.rel.late.operatoratatime.Operator
+import ch.epfl.dias.cs422.helpers.rel.early.volcano.Operator
 import ch.epfl.dias.cs422.helpers.{PrintUtil, SqlPrepare}
-import org.apache.commons.lang3.exception.ExceptionUtils
-import org.slf4j.LoggerFactory
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val sql = "select MAX(col1) from order_small"
-    /*"""select
+    val sql = """
+select
     l_returnflag,
     l_linestatus,
     sum(l_quantity) as sum_qty,
     sum(l_extendedprice) as sum_base_price,
     sum(l_extendedprice * (1 - l_discount)) as sum_disc_price,
-    sum(l_extendedprice * (1 - l_discount) * (1 + l_tax)) as sum_charge,
+    sum(l_extendedprice  (1 - l_discount)  (1 + l_tax)) as sum_charge,
     avg(l_quantity) as avg_qty,
     avg(l_extendedprice) as avg_price,
     avg(l_discount) as avg_disc,
@@ -31,29 +29,25 @@ order by
     l_returnflag,
     l_linestatus
       """
-*/
-    try {
-      val prep = SqlPrepare(Factories.LAZY_OPERATOR_AT_A_TIME_INSTANCE, "rowstore")
-      val rel = prep.prepare(sql)
 
-      PrintUtil.printTree(rel)
+    val prep = SqlPrepare(Factories.VOLCANO_INSTANCE, "rowstore")
+    val rel = prep.prepare(sql)
 
-      for (i <- 1 to 1) {
-        println("Iteration " + i + " :")
-        rel.asInstanceOf[Operator].foreach(println)
-        //      equivalent:
-        //      rel.open()
-        //      breakable {
-        //        while (true) {
-        //          val n = rel.next()
-        //          if (n == Nil) break // It's not safe to call next again after it returns Nil
-        //          println(n)
-        //        }
-        //      }
-        //      rel.close()
-      }
-    }catch {
-      case x : Exception => x.printStackTrace()
+    PrintUtil.printTree(rel)
+
+    for (i <- 1 to 10) {
+      println("Iteration " + i + " :")
+      rel.asInstanceOf[Operator].foreach(println)
+      //      // equivalent:
+      //      rel.open()
+      //      breakable {
+      //        while (true) {
+      //          val n = rel.next()
+      //          if (n == Nil) break // It's not safe to call next again after it returns Nil
+      //          println(n)
+      //        }
+      //      }
+      //      rel.close()
     }
   }
 }
